7c47b3c2302d6461f695e211e2739867f6b46386
kendo1234 <kendomustdie@googlemail.com>
2018-06-25 17:29:55 +0100

    Protoss easy difficulty victory

--- starcraft2.py
+++ starcraft2.py
@@ -1,7 +1,8 @@
 import sc2
 from sc2 import run_game, maps, Race, Difficulty
 from sc2.player import Bot, Computer
-from sc2.constants import NEXUS, PROBE, PYLON, ASSIMILATOR
+from sc2.constants import NEXUS, PROBE, PYLON, ASSIMILATOR, GATEWAY, CYBERNETICSCORE, STALKER
+import random
 
 
 class SentdeBot(sc2.BotAI):
@@ -11,7 +12,10 @@ class SentdeBot(sc2.BotAI):
         await self.build_workers()
         await self.build_pylons()
         await self.build_assimilators()
-        await self.expand() # not using expand_now method in bot_ai for logical reasons Vs AI opponent
+        await self.expand()  # not using expand_now method in bot_ai for logical reasons Vs AI opponent
+        await self.offensive_force_buildings()
+        await self.build_offensive_force()
+        await self.attack()
 
     async def build_workers(self):
         for nexus in self.units(NEXUS).ready.noqueue:
@@ -42,6 +46,43 @@ class SentdeBot(sc2.BotAI):
         # customise method of expansion
         if self.units(NEXUS).amount < 3 and self.can_afford(NEXUS):
             await self.expand_now()
+
+    async def offensive_force_buildings(self):
+        if self.units(PYLON).ready.exists:
+            pylon = self.units(PYLON).ready.random
+
+            if self.units(GATEWAY).ready.exists and not self.units(CYBERNETICSCORE):
+                if self.can_afford(CYBERNETICSCORE) and not self.already_pending(CYBERNETICSCORE):
+                    await self.build(CYBERNETICSCORE, near=pylon)
+
+            elif len(self.units(GATEWAY)) < 3:
+                if self.can_afford(GATEWAY) and not self.already_pending(GATEWAY):
+                    await self.build(GATEWAY, near=pylon)
+
+    async def build_offensive_force(self):
+        for gw in self.units(GATEWAY).ready.noqueue:
+            if self.can_afford(STALKER) and self.supply_left > 0:
+                await self.do(gw.train(STALKER))
+
+    # FUNCTION PRIORITISES KNOWN UNITS AND STRUCTURES FOR ATTACK STEP WHEN UNITS REACH HIGH ENOUGH STRENGTH
+    def find_target(self, state):
+        if len(self.known_enemy_units) > 0:
+            return random.choice(self.known_enemy_units)
+        elif len(self.known_enemy_structures) > 0:
+            return random.choice(self.known_enemy_structures)
+        else:
+            return self.enemy_start_locations[0]
+
+    async def attack(self):
+        # Go for the eyes BOO!!
+        if self.units(STALKER).amount > 15:
+            for s in self.units(STALKER).idle:
+                await self.do(s.attack(self.find_target(self.state)))
+        # attack at random
+        if self.units(STALKER).amount > 3:
+            if len(self.known_enemy_units) > 0:
+                for s in self.units(STALKER).idle:
+                    await self.do(s.attack(random.choice(self.known_enemy_units)))
 
 
 run_game(maps.get("AbyssalReefLE"), [
